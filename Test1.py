807be65f78ad3a7131e171441ddbd88996983049
THOXIV <68148697+THOXIV@users.noreply.github.com>
2024-07-02 17:09:59 +0200

    Add files via upload

--- Test1.py
+++ Test1.py
@@ -2,30 +2,26 @@ Hrac1Skore = 0
 Hrac2Skore = 0
 VzalenostHrac1 = 10
 VzalenostHrac2 = 0
-
-
-def pocetb():
-    global Hrac1Skore
-    global Hrac2Skore
-    if Vzalenost > 10:
-        Hrac1Skore += 1
-        print(f"Hráč 1 má skóre:{Hrac1Skore} a Hráč 2 má skóre:{Hrac2Skore} a vzdálenost je {Vzalenost}")
-    elif Vzalenost < 0:
-        Hrac2Skore += 1
-        print(f"Hráč 1 má skóre:{Hrac1Skore} a Hráč 2 má skóre:{Hrac2Skore} a vzdálenost je {Vzalenost}")
+Vzalenost = 5
 
 def vypocetBodu(x):
     x += 1
     print(f"Hráč 1 má skóre:{Hrac1Skore} a Hráč 2 má skóre:{Hrac2Skore} a vzdálenost je {Vzalenost}")
     return x
 
+def resetVzdalenost(V=5):
+    print(f"Vzdálenost je {V}")
+    return V
+
 if __name__ == "__main__":
 
     while Hrac1Skore < 10 and Hrac2Skore < 10:
-        Vzalenost = int(input("Vzdálenost je:"))
-
-
+        Vzalenost += int(input(f"Vzdálenost je:{Vzalenost}+"))
         if Vzalenost > VzalenostHrac1:
             Hrac1Skore = vypocetBodu(Hrac1Skore)
+            Vzalenost = resetVzdalenost(5)
         elif Vzalenost < VzalenostHrac2:
             Hrac2Skore = vypocetBodu(Hrac2Skore)
+            Vzalenost = resetVzdalenost(5)
+
+
